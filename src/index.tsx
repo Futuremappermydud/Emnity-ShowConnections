24cb4faf8575616902a6cc3427f317c38ce07859
Futuremappermydud <54294576+Futuremappermydud@users.noreply.github.com>
2023-08-21 19:24:32 -0400

    update

--- src/index.tsx
+++ src/index.tsx
@@ -6,10 +6,8 @@ import manifest from '../manifest.json';
 
 import Settings from './components/Settings';
 
-const Typing = getByProps('startTyping');
 const Patcher = create('show-connections');
 const UserProfile = getByProps("PRIMARY_INFO_TOP_OFFSET", "SECONDARY_INFO_TOP_MARGIN", "SIDE_PADDING")
-const UserTheme = getByProps('theme', 'primaryColor');
 import { findInReactTree } from "enmity/utilities"
 import Connections from './components/Connections';
 
@@ -17,20 +15,22 @@ const ShowConnections: Plugin = {
    ...manifest,
 
    onStart() {
-      Patcher.instead(Typing, 'startTyping', () => { });
-      Patcher.instead(Typing, 'stopTyping', () => { });
       Patcher.after(UserProfile.default, "type", (_, __, res) => {
          const profileCardSection = findInReactTree(res, r => 
              r?.type?.displayName === "View" &&
              r?.props?.children.findIndex(i => i?.type?.name === "UserProfileBio") !== -1
          )?.props?.children
 
+         const userProfileTheme = findInReactTree(res, r => 
+            r?.type?.displayName === "ThemeContextProvider"
+        )?.props?.theme
+
          if (!profileCardSection) return res;
 
          const { userId } = profileCardSection?.find((r: any) => typeof r?.props?.displayProfile?.userId === "string")?.props?.displayProfile ?? {};
          
 
-         profileCardSection.unshift(<Connections userId={userId} theme="light"/>)
+         profileCardSection.unshift(<Connections userId={userId} theme={userProfileTheme}/>)
      })
    },
 
