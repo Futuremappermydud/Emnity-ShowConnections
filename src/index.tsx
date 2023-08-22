a45518f97cffe9c07c2352cf46973aa1c003be07
Futuremappermydud <54294576+Futuremappermydud@users.noreply.github.com>
2023-08-22 18:29:04 -0400

    test

--- src/index.tsx
+++ src/index.tsx
@@ -13,17 +13,52 @@ import { findInReactTree } from "enmity/utilities"
 import Connections from './components/Connections';
 import { getBoolean } from 'enmity/api/settings';
 
+function encode(primary: number, accent: number): string {
+   const message = `[#${primary.toString(16).padStart(6, "0")},#${accent.toString(16).padStart(6, "0")}]`;
+   const padding = "";
+   const encoded = Array.from(message)
+       .map(x => x.codePointAt(0))
+       .filter(x => x! >= 0x20 && x! <= 0x7f)
+       .map(x => String.fromCodePoint(x! + 0xe0000))
+       .join("");
+
+   return (padding || "") + " " + encoded;
+}
+
+// Courtesy of Cynthia.
+function decode(bio: string): Array<number> | null {
+   if (bio == null) return null;
+
+   const colorString = bio.match(
+       /\u{e005b}\u{e0023}([\u{e0061}-\u{e0066}\u{e0041}-\u{e0046}\u{e0030}-\u{e0039}]+?)\u{e002c}\u{e0023}([\u{e0061}-\u{e0066}\u{e0041}-\u{e0046}\u{e0030}-\u{e0039}]+?)\u{e005d}/u,
+   );
+   if (colorString != null) {
+       const parsed = [...colorString[0]]
+           .map(x => String.fromCodePoint(x.codePointAt(0)! - 0xe0000))
+           .join("");
+       const colors = parsed
+           .substring(1, parsed.length - 1)
+           .split(",")
+           .map(x => parseInt(x.replace("#", "0x"), 16));
+
+       return colors;
+   } else {
+       return null;
+   }
+}
+
 const ShowConnections: Plugin = {
    ...manifest,
 
    onStart() {
       Patcher.instead(UserProfileStore, 'getUserProfile', (self, args, res) => {
-         console.log('hihihi');
-         console.log(JSON.stringify(args));
          let result = res.apply(self, args);
-         result.themeColors = [4403745, 11112810];
-         result.premiumType = 2;
-         console.log(JSON.stringify(result));
+         if (true && result.themeColors) return result;
+            const colors = decode(result.bio);
+            if (colors) {
+               result.themeColors = colors;
+               result.premiumType = 2;
+            }
          return result;
       });
 
